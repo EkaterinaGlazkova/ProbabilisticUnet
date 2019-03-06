68895fe0a459f1b1acef72969c012215eac99e2a
EkaterinaGlazkova <catherine.glazkova@gmail.com>
2019-03-06 21:09:07 +0000

    visualization notebook changed

--- training.py
+++ training.py
@@ -7,10 +7,6 @@ import numpy as np
 import PIL
 import matplotlib
 
-matplotlib.use('agg')
-
-import matplotlib.pyplot as plt
-
 from model.model import ProbUNet
 import visualization_utils
 
@@ -19,7 +15,7 @@ from labels import labels
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 num_classes = 19
 
-batch_size = 16
+batch_size = 8
 latent_space_size = 6
 
 
@@ -56,7 +52,8 @@ def train_epoch(model, optimizer, train_loader, batchsize):
     return loss_log
 
 def test(model, epoch_num, test_loader, res_dir = None):
-    ce_log, acc_log = [], []
+    #ce_log, acc_log = [], []
+    ce_log = []
     model.eval()
     for batch_num, (x_batch, y_batch) in zip(trange(len(test_loader)), test_loader):  
         x_batch = x_batch.to(device)
@@ -68,26 +65,26 @@ def test(model, epoch_num, test_loader, res_dir = None):
         ce_log.append(loss)
         
         pred = torch.argmax(output.permute(0,2,3,1), dim = -1)
-        acc = torch.eq(pred, y_batch).float().mean().cpu().numpy()
-        acc_log.append(acc)
-        
+        #acc = torch.eq(pred, y_batch).float().mean().cpu().numpy()
+        #acc_log.append(acc)
         
         if res_dir and batch_num == 0:
             visualization_utils.plot_batch_with_results(x_batch, 
                                                         y_batch.cpu().view(batch_size, -1, 1, 256, 512), 
                                                         pred.cpu().view(batch_size, -1, 1, 256, 512), 
                                                         outdir = res_dir + "epoch_{}.pdf".format(epoch_num))
-    return ce_log, acc_log
+    return ce_log#, acc_log
 
-def plot_history(train_history, ce_log, acc_log, epoch_num, save_dir = "results/"):
+#def plot_history(train_history, ce_log, acc_log, epoch_num, save_dir = "results/"):
+def plot_history(train_history, ce_log, epoch_num, save_dir = "results/"):
     plt.figure()
     plt.title('Results for epoch {}'.format(epoch_num))
     plt.plot(train_history, label='train', zorder=1)
     
     points = np.array(ce_log)
     plt.scatter(points[:, 0], points[:, 1], marker='+', s=180, c='orange', label='val_ce', zorder=2)
-    points = np.array(acc_log)
-    plt.scatter(points[:, 0], points[:, 1], marker='+', s=180, c='orange', label='val_acc', zorder=2)
+    #points = np.array(acc_log)
+    #plt.scatter(points[:, 0], points[:, 1], marker='+', s=180, c='orange', label='val_acc', zorder=2)
         
     plt.xlabel('train steps')
 
@@ -99,30 +96,35 @@ def plot_history(train_history, ce_log, acc_log, epoch_num, save_dir = "results/
     
 def train(model, opt, n_epochs, train_loader, test_loader, save_path = None):
     train_log = []
-    ce_log, acc_log = [], []
+    #ce_log, acc_log = [], []
+    ce_log = []
 
     batchsize = batch_size
 
     for epoch in range(n_epochs):
         train_loss = train_epoch(model, opt, train_loader, batchsize=batchsize)
 
-        val_loss, val_acc = test(model, epoch, test_loader, res_dir = "results/")
+        #val_loss, val_acc = test(model, epoch, test_loader, res_dir = "results/")
+        val_loss = test(model, epoch, test_loader, res_dir = "results/")
         train_log.extend(train_loss)
 
         steps = len(train_loader)
         ce_log.append((steps * (epoch + 1), np.mean(val_loss)))
-        acc_log.append((steps * (epoch + 1), np.mean(val_acc)))
+        #acc_log.append((steps * (epoch + 1), np.mean(val_acc)))
 
-        plot_history(train_log, ce_log, acc_log, epoch, "results/")    
+        #plot_history(train_log, ce_log, acc_log, epoch, "results/")  
+        plot_history(train_log, ce_log,  epoch, "results/")  
         if save_path:
             torch.save(model.state_dict(), save_path)
             
-    print("Final error: {:.2%}".format(1 - acc_log[-1][1]))
+    #print("Final error: {:.2%}".format(1 - acc_log[-1][1]))
     
 def training():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     data_root = "/home/glazkova/ProbabilisticUnet/data"
     id_to_train_id = get_id_to_train_id()
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
 
     img_transform_func = transforms.Compose([
                         transforms.Resize((256, 512), interpolation = PIL.Image.BILINEAR),
@@ -165,7 +167,7 @@ def training():
 
     model = ProbUNet(num_classes, latent_space_size)
     model.to(device)
-    opt = torch.optim.Adam(model.parameters(), lr=0.001)
+    opt = torch.optim.Adam(model.parameters(), lr=0.00001)
     train(model, opt, n_epochs, train_loader, test_loader, save_path = "results/model")
 
 if __name__ == "__main__":
